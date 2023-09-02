3ae3ba470f6863c329484b3f930acdb7365f3876
LeeMiHyun <mh1217t@gmail.com>
2023-09-02 20:16:22 +0900

    커밋

---
+++ src/test/java/com/prj/tsi/TsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prj.tsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
