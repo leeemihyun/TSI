73ccf134689ced77dc731123af8e4016925bc879
leeemihyun <mihyun961217@naver.com>
2023-09-30 22:09:24 +0900

    FEAT : mainController annotation

--- src/main/java/com/prj/tsi/mainController.java
+++ src/main/java/com/prj/tsi/mainController.java
@@ -1,10 +1,13 @@
 package com.prj.tsi;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class mainController {
 
+    @GetMapping("main")
     public String main(){ return "main"; }
 
 }
