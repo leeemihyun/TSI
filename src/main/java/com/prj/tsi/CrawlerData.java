55ad9f3940c27a6db8e14fe4658d36ded12f5588
leeminsu <ooori100@naver.com>
2023-11-11 21:59:00 +0900

    FEAT : 보배,클리앙 크롤링 메소드 완성

--- src/main/java/com/prj/tsi/CrawlerData.java
+++ src/main/java/com/prj/tsi/CrawlerData.java
@@ -75,7 +75,34 @@ public class CrawlerData {
             Elements writeDate=doc.select("#boardlist > tbody > tr > td.date "); //작성 시간
             Elements index=doc.select("#boardlist > tbody > tr"); //갯수돌리는
             for(int i=0;i<index.size();i++){
-                System.out.println("hi");
+                //System.out.println(index.get(i).text());
+                String titleName=titles.get(i).text();//제목
+                String url="https://www.bobaedream.co.kr/"+titles.get(i).attr("href");//링크
+
+                String dataNum=url.substring(url.indexOf("No="),url.indexOf("&vdate="));
+
+                String commentCountTemp=commCnt.get(i).text();//댓글 수
+                String viewCountTemp=viewCnt.get(i).text();//조회 수
+                int commentCount=0;
+
+                try {
+                    commentCount=Integer.parseInt(commentCountTemp);
+                }catch (NumberFormatException nfe){
+                    commentCount=0;
+                }
+
+                int viewCount=0;
+                try {
+                    viewCount=Integer.parseInt(viewCountTemp);
+                }catch (NumberFormatException nfe){
+                    viewCount=0;
+                }
+
+
+                String writerName=writer.get(i).text();
+                String writingTime=writeDate.get(i).text();
+                DataVO dataVO=new DataVO(url,writingTime,writerName,titleName,"bobae",dataNum,viewCount,commentCount);
+                list.add(dataVO);
             }
             //#boardlist > tbody > tr:nth-child(1)
         }catch (IOException e){
@@ -93,7 +120,53 @@ public class CrawlerData {
     public static List<DataVO> clienGetData() throws IOException {
         List<DataVO> list =new ArrayList<>();
         try {
-            Document doc= Jsoup.connect("https://www.bobaedream.co.kr/list?code=best&type=list&page=2").get(); //html 가져오기
+            Document doc= Jsoup.connect("https://www.clien.net/service/board/park").get(); //html 가져오기
+            Elements titles=doc.select("#div_content > div.list_content  > div > div.list_title > a.list_subject"); //제목,링크, 링크안에 숫자가 데이터번호
+            Elements viewCnt=doc.select("#div_content > div.list_content  > div  > div.list_hit > span"); //조회 수
+            Elements commCnt=doc.select("#div_content > div.list_content  > div  > div.list_title > a.list_reply.reply_symph > span"); //댓글 수
+            Elements writer=doc.select("#div_content > div.list_content  > div  > div.list_author > span.nickname"); //작성자
+            Elements writeDate=doc.select("#div_content > div.list_content  > div  > div.list_time > span > span"); //작성 시간
+            //URL,글쓴시간,작성자,제목명,커뮤니티명,데이터번호,조회수,댓글수
+            Elements index=doc.select("#div_content > div.list_content > div"); //갯수돌리는
+            for(int i=0;i<index.size();i++){
+                String titleName=titles.get(i).text(); //제목
+                String viewCount=viewCnt.get(i).text(); //조회수
+
+                int viewCountTemp=0;
+
+                try {
+                    viewCountTemp=Integer.parseInt(viewCount);
+                }catch (NumberFormatException nfe){
+                    viewCountTemp=0;
+                }
+
+
+                String url="https://www.clien.net"+titles.get(i).attr("href"); //url
+                String dataNum=url.substring(url.indexOf("park/")+5,url.indexOf("?"));
+                Elements temp=index.get(i).select("div.list_title > a.list_reply.reply_symph > span"); //댓글 수
+                String commCntTemp=temp.text();
+                if(commCntTemp.equals("")){
+                  commCntTemp="0";
+                }
+
+                int commentCount=0;
+                try {
+                    commentCount=Integer.parseInt(commCntTemp);
+                }catch (NumberFormatException nfe){
+                    commentCount=0;
+                }
+
+
+                String writerTemp=writer.get(i).text();
+                if(writerTemp.equals("")){
+                    writerTemp=writer.select("img").attr("title");
+                }
+
+                String writeTime=writeDate.get(i).text(); //글쓴시간
+
+                DataVO dataVO=new DataVO(url,writeTime,writerTemp,titleName,"clien",dataNum,viewCountTemp,viewCountTemp);
+                list.add(dataVO);
+            }
         }catch (IOException e){
             e.printStackTrace();
         }
@@ -110,7 +183,18 @@ public class CrawlerData {
     public static List<DataVO> etlandGetData() throws IOException {
         List<DataVO> list =new ArrayList<>();
         try {
-            Document doc= Jsoup.connect("https://www.bobaedream.co.kr/list?code=best&type=list&page=2").get(); //html 가져오기
+            Document doc= Jsoup.connect("https://www.etoland.co.kr/pages/pop.php?&page=1").get(); //html 가져오기
+
+            Elements index=doc.select("body > main > section > div.board_new2_wrap > ul > li:not( .middle_line):not(.ad):not(.ad):not(.board_new2_title):not(.ad_list)"); //갯수돌리는
+
+            for(int i=0;i<index.size();i++){
+                System.out.println(index.get(i).text());
+                System.out.println("https://www.etoland.co.kr"+index.get(i).select("div.subject > a ").attr("href"));
+                System.out.println(index.get(i).select("div.subject > a ").text());
+                //body > main > section > div.board_new2_wrap > ul > li:nth-child(6) > div.subject > a > span
+                System.out.println("-----------------------------");
+            }
+
         }catch (IOException e){
             e.printStackTrace();
         }
@@ -200,8 +284,11 @@ public class CrawlerData {
 
 
     public static void main(String[] args) throws IOException {
-        List<DataVO> list=CrawlerData.cook82GetData();
-        System.out.println(list);
+        //List<DataVO> list=CrawlerData.cook82GetData();
+        //List<DataVO> list=CrawlerData.clienGetData();
+        //List<DataVO> list=CrawlerData.bobaeGetData();
+        etlandGetData();
+        //System.out.println(list);
     }
 
 
